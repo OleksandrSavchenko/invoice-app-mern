6bb7d45e1aff0cfc1b0a5eaffd08c3f418e9a137
OleksandrSavchenko <oleksandr.savchenko3@gmail.com>
2020-03-09 13:00:27 +0200

    FE fixed.

--- client/src/components/LinkCard.js
+++ client/src/components/LinkCard.js
@@ -3,12 +3,12 @@ import React from 'react'
 export const LinkCard = ({ link }) => {
   return (
     <>
-      <h2>Ссылка</h2>
+      <h2>Link</h2>
 
-      <p>Ваша ссылка: <a href={link.to} target="_blank" rel="noopener noreferrer">{link.to}</a></p>
-      <p>Откуда: <a href={link.from} target="_blank" rel="noopener noreferrer">{link.from}</a></p>
-      <p>Количество кликов по ссылке: <strong>{link.clicks}</strong></p>
-      <p>Дата создания: <strong>{new Date(link.date).toLocaleDateString()}</strong></p>
+      <p>Your link: <a href={link.to} target="_blank" rel="noopener noreferrer">{link.to}</a></p>
+      <p>From: <a href={link.from} target="_blank" rel="noopener noreferrer">{link.from}</a></p>
+      <p>Link's clicks count: <strong>{link.clicks}</strong></p>
+      <p>Created Date: <strong>{new Date(link.date).toLocaleDateString()}</strong></p>
     </>
   )
 }
