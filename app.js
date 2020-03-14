3f6c374273adac42020a9d357cc962cf0e2e1fdb
OleksandrSavchenko <oleksandr.savchenko3@gmail.com>
2020-03-15 00:18:44 +0200

    Useless code removed.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const config = require('config');
-// const path = require('path');
 const mongoose = require('mongoose');
 const argv = require('minimist')(process.argv.slice(2));
 
@@ -12,14 +11,6 @@ app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/link', require('./routes/link.routes'));
 app.use('/t', require('./routes/redirect.routes'));
 app.use('/api/test', require('./routes/test.routes'));
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use('/', express.static(path.join(__dirname, 'client', 'build')));
-//
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
 
 const PORT = argv.port || config.get('port') || 5000;
 
